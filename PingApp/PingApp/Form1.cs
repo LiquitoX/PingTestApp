1b2d69a91a7b2dde3b3c2c7d661768d1794ae2ab
LiquitoX <wenzelliska@hotmail.com>
2016-09-03 20:54:32 +0200

    Made visual changes and created a testloop. Added closebutton and some comments

--- PingApp/PingApp/Form1.cs
+++ PingApp/PingApp/Form1.cs
@@ -22,21 +22,94 @@ namespace PingApp
             
         }
 
-        private void button1_Click(object sender, EventArgs e){
-            listBox1.Items.Clear();
-            for (int i = 0; i < 4; i++){
-                using (Ping p = new Ping()){
-                    listBox1.Items.Add("Ping is: " + p.Send(hostName.Text).RoundtripTime.ToString() + "ms\n");
+        //
+        // Activates the pingTimer and enabeles the "stop" button
+        //        
+        private void testIP_Click(object sender, EventArgs e){
+            clearListBox();
+            pingTimer.Enabled = true;
+            testIP.Enabled = false;
+            StopIPTest.Enabled = true;
+            btnClose.Enabled = false;
+        }
 
-                }
+        //
+        // Stops the pingTimer and activates the "start" button
+        //
+        private void StopIPTest_Click(object sender, EventArgs e){
+            pingTimer.Enabled = false;
+            listBox1.Items.Add("Test stopped!");
+            selectLastItem();
+            testIP.Enabled = true;
+            StopIPTest.Enabled = false;
+            btnClose.Enabled = true;
+
+        }
+        /// <summary>
+        /// pingTest()
+        ///     Pastes the 4 inputboxes together to one string and then 
+        ///     tests if there is a pingable connection between the host
+        ///     and the given IP-Address. The results are displayed in
+        ///     an listbox.
+        /// </summary>
+        private void pingTest(){
+            string ip =
+                hostIP1.Text + "." +
+                hostIP2.Text + "." +
+                hostIP3.Text + "." +
+                hostIP4.Text;
+            Ping ping = new Ping();
+
+            PingReply pingStatus = ping.Send(IPAddress.Parse(ip));
+
+            if (pingStatus.Status == IPStatus.Success){
+                listBox1.Items.Add("Ping to " + ip + " was successful");
+            } else {
+                listBox1.Items.Add("Ping has Failed!");
+                pingTimer.Enabled = false;
+                MessageBox.Show("ERRORCODE: 01\n" + "Ping was not resolved!", "ERROR",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                resetTest();
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            pingTest();
+        /// <summary>
+        /// Clears all the listbox items.
+        /// </summary>
+        private void clearListBox(){
+            listBox1.Items.Clear();
         }
-
+        
+        /// <summary>
+        /// When the length is 3 or greater, move to the next inputfield.
+        /// </summary>
+        private void hostIP1_TextChanged(object sender, EventArgs e){
+            validateText(1);
+            int len = hostIP1.TextLength;
+            if (len >= 3){
+                hostIP2.Focus();
+            }
+        }
+        private void hostIP2_TextChanged(object sender, EventArgs e){
+            validateText(2);
+            int len = hostIP2.TextLength;
+            if (len >= 3){
+                hostIP3.Focus();
+            }
+        }
+        private void hostIP3_TextChanged(object sender, EventArgs e){
+            validateText(3);
+            int len = hostIP3.TextLength;
+            if (len >= 3){
+                hostIP4.Focus();
+            }
+        }
+        private void hostIP4_TextChanged(object sender, EventArgs e){
+            validateText(4);
+            int len = hostIP4.TextLength;
+            if (len >= 3){
+                hostIP1.Focus();
+            }
+        }
         private void hostIP1_Enter(object sender, EventArgs e){
             hostIP1.SelectAll();
         }
@@ -50,61 +123,65 @@ namespace PingApp
             hostIP4.SelectAll();
         }
 
-        private void hostIP4_TextChanged(object sender, EventArgs e){
-            int len = hostIP4.TextLength;
-            if (len >= 3){
-                pingTest();
-                hostIP1.Focus();
-            }
+        private void pingTimer_Tick(object sender, EventArgs e)
+        {
+            pingTest();
+            selectLastItem();
         }
 
-
-        private void pingTest()
-        {
-            string ip =
-                hostIP1.Text + "." +
-                hostIP2.Text + "." +
-                hostIP3.Text + "." +
-                hostIP4.Text;
-            Ping ping = new Ping();
-
-            PingReply pingStatus = ping.Send(IPAddress.Parse(ip));
-
-            if (pingStatus.Status == IPStatus.Success)
-            {
-                listBox1.Items.Add("Ping to " + ip + " was successful");
-            }
-            else
-            {
-                listBox1.Items.Add("Ping has Failed!");
-            }
+        private void Form1_Load(object sender, EventArgs e){
+            hostIP1.Focus();
+            StopIPTest.Enabled = false;
+        }
+        private void resetTest(){
+            StopIPTest.Enabled = false;
+            testIP.Enabled = true;
+            btnClose.Enabled = true;
         }
 
-        private void hostIP3_TextChanged(object sender, EventArgs e)
+        
+        private void selectLastItem(){
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+        }
+        private void validateText(int Number)
         {
-            int len = hostIP3.TextLength;
-            if (len >= 3)
-            {
-                hostIP4.Focus();
+            if (Number == 1){
+                if (System.Text.RegularExpressions.Regex.IsMatch(hostIP1.Text, "[^0-9]")){
+                    hostIP1.Text = "";
+                    MessageBox.Show("Please enter only numbers.");
+                    
+                }
+            } else if (Number == 2){
+                if (System.Text.RegularExpressions.Regex.IsMatch(hostIP2.Text, "[^0-9]")){ 
+                    MessageBox.Show("Please enter only numbers.");
+                    hostIP2.Text = "";
+                }
+            } else if (Number == 3){
+                if (System.Text.RegularExpressions.Regex.IsMatch(hostIP3.Text, "[^0-9]")){
+                    MessageBox.Show("Please enter only numbers.");
+                    hostIP3.Text = "";
+                }
+            } else if (Number == 4) {
+                if (System.Text.RegularExpressions.Regex.IsMatch(hostIP4.Text, "[^0-9]")){ 
+                    MessageBox.Show("Please enter only numbers.");
+                    hostIP4.Text = "";
+                }
             }
+            
         }
 
-        private void hostIP2_TextChanged(object sender, EventArgs e)
+        /// <summary>
+        /// Exits the program
+        /// </summary>
+        private void btnClose_Click(object sender, EventArgs e)
         {
-            int len = hostIP2.TextLength;
-            if (len >= 3)
-            {
-                hostIP3.Focus();
-            }
+            Application.Exit();
         }
 
-        private void hostIP1_TextChanged(object sender, EventArgs e)
+        private void btnAbout_Click(object sender, EventArgs e)
         {
-            int len = hostIP1.TextLength;
-            if (len >= 3)
-            {
-                hostIP2.Focus();
-            }
+            // TODO: Implement About page.
+            MessageBox.Show("Function not implemented yet!");
         }
     }
 }
