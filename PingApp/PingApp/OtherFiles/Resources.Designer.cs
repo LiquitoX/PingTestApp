7c844addb69d236d84ecec227b9fc5422aa65143
LiquitoX <wenzelliska@hotmail.com>
2016-09-12 23:24:24 +0200

    Moved some files, added an changelog
    - For more info, check the changelog for updates to the app.

--- PingApp/PingApp/OtherFiles/Resources.Designer.cs
+++ PingApp/PingApp/OtherFiles/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PingApp.Properties {
+namespace PingApp.OtherFiles {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PingApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PingApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PingApp.OtherFiles.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
